3cd33183eba50deb90a190d988162f93deeea65f
Costin F <costinfl@users.noreply.github.com>
2023-10-31 10:05:04 +0200

    week_05 complete

--- Week_05/Exercise/Code/ui/src/app/app-routing.module.ts
+++ Week_05/Exercise/Code/ui/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AuthGuard} from 'src/app/guards/auth.guard';
+import {LoginPageComponent} from './pages/login-page/login-page.component';
+import {RegisterPageComponent} from './pages/register-page/register-page.component';
+import {DashboardPageComponent} from './pages/dashboard-page/dashboard-page.component';
+import {NotFoundPageComponent} from './pages/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'register', component: RegisterPageComponent},
+  {path: 'dashboard', component: DashboardPageComponent, canActivate: [AuthGuard]},
+  {path: '**', component: NotFoundPageComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
